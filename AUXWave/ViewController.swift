9bdd6ed07f5c1895f47942b79cbb5ebd6c9eac3c
ncvitak <ncvitak@gmail.com>
2015-02-28 23:17:18 -0500

    Added Xcode Project
    Signed-off-by: ncvitak <ncvitak@gmail.com>

---
+++ AUXWave/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AUXWave
+//
+//  Created by Nico Cvitak on 2015-02-28.
+//  Copyright (c) 2015 UW-AppDEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
